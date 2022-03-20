3459a255cdd3dd5469342f0d8bfe58b973afc477
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-20 15:41:15 -0300

    feat: Echo GET Data

---
+++ backend/api/views.py
@@ -0,0 +1,15 @@
+import json
+from django.http import JsonResponse
+
+def api_home(request, *args, **kwargs):
+  body = request.body
+  data = {}
+  try: 
+    data = json.loads(body)
+  except:
+    pass
+  print(data)
+  data['params'] = dict(request.GET)
+  data['headers'] = dict(request.headers)
+  data['content_type'] = request.content_type
+  return JsonResponse({"message": "Hi there, I'm API"})
