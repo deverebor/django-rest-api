dd0f53d83184797ed22bdeae14041620ddaeffa8
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-20 18:50:00 -0300

    feat: Django rest_framework model serializers

---
+++ backend/products/serializers.py
@@ -0,0 +1,20 @@
+from pickletools import read_long1
+from rest_framework import serializers
+
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+  my_discount = serializers.SerializerMethodField(read_only=True)
+  class Meta:
+    model = Product
+    fields = [
+      'title',
+      'content',
+      'price',
+      'sale_price',
+      'my_discount',
+    ]
+    
+  def get_my_discount(self, obj):
+    return obj.get_discount()
+    
