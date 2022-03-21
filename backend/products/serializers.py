5a819959c8aa4379a6931ecc61a61e9a8682e6d7
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-21 19:55:05 -0300

    feat: session authentication and permissions

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -4,23 +4,21 @@ from rest_framework import serializers
 from .models import Product
 
 
-def get_my_discount(obj):
-    if not hasattr(obj, 'id'):
-        return None
-    if not isinstance(obj, Product):
-        return None
-    return obj.get_discount()
-
-
 class ProductSerializer(serializers.ModelSerializer):
-    my_discount = serializers.SerializerMethodField(read_only=True)
+  my_discount = serializers.SerializerMethodField(read_only=True)
+  class Meta:
+    model = Product
+    fields = [
+      'title',
+      'content',
+      'price',
+      'sale_price',
+      'my_discount',
+    ]
 
-    class Meta:
-        model = Product
-        fields = [
-            'title',
-            'content',
-            'price',
-            'sale_price',
-            'my_discount',
-        ]
+  def get_my_discount(self, obj):
+    if not hasattr(obj, 'id'):
+      return None
+    if not isinstance(obj, Product):
+      return None
+    return obj.get_discount()
