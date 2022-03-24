1407e6c81560f5f3d9dd7336214f2e02603d8296
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-23 21:08:02 -0300

    feat: DRF authentications and tokens

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,18 +3,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
+from api.authentication import TokenAuthentication
+
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [
+        authentication.SessionAuthentication,
+        TokenAuthentication,
+    ]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
-        print(serializer.validated_data)
+        # serializer.save(user = self.request.user)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
@@ -37,6 +43,7 @@ product_detail_view = ProductDetailAPIView.as_view()
 class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
     lookup_field = 'pk'
 
     def perform_update(self, serializer):
--- py-client/create.py
+++ py-client/create.py
@@ -1,10 +1,12 @@
 import requests
 
+headers = {'Authorization': 'Bearer 4cd1b38f30f5fcefffea2856d450ecff461ec36f'}
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
   "title": "The Catcher in the Rye",
+  "price": "12.99",
 }
 
-get_response = requests.post(endpoint, data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
